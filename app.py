8d07d0a2dd73361367e3ef1739cbee1dee934c7c
Mohammad Mansib Newaz <mohammad.newaz1@northsouth.edu>
2025-02-06 21:02:34 +0600

    Enhance image and text feature encoding with normalization and probability calculation

--- app.py
+++ app.py
@@ -8,5 +8,14 @@ model.eval()
 tokenizer = open_clip.load("openai/clip-vit-base-patch32")
 
 image=preprocess(Image.open("image.jpg")).unsqueeze(0)
-text = tokenizer.encode("a photo of a
-                        
+text = tokenizer.encode("a photo of a")
+
+with torch.no_grad(), torch.cuda.amp.autocast():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+
+    text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+print("Label probs:", text_probs)                        
