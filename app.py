97f3855acbf918d3d36f1343e4da8b9d5224d3d7
Mohammad Mansib Newaz <mohammad.newaz1@northsouth.edu>
2025-02-06 21:06:30 +0600

    Fix typo in model and transforms creation in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import open_clip 
 
 
-model,_, preprocess = open_clip..create_model_and_transforms
+model,_, preprocess = open_clip.create_model_and_transforms
 model.eval()
 tokenizer = open_clip.load("openai/clip-vit-base-patch32")
 
